32c50f45cd4f34edfbb7206bb225b87a3993b0dd
gusutaharu <j.gusutahu.arufonnsu@gmail.com>
2026-03-26 22:54:40 +0900

    feat: 新規投稿ページの検証機能実装

--- app/bbs-posts/create/page.tsx
+++ app/bbs-posts/create/page.tsx
@@ -1,7 +1,84 @@
-const createPage = () => {
+"use client";
+import { Field, FieldError, FieldLabel } from "@/components/ui/field";
+import { Textarea } from "@/components/ui/textarea";
+import { Input } from "@/components/ui/input";
+import { formSchema } from "@/schema/formSchema";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { Button } from "@/components/ui/button";
+
+function CreatePage() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      title: "",
+      content: "",
+    },
+  });
+
+  async function onSubmit() {}
+
   return (
-    <div>create</div>
-  )
+    <form
+      onSubmit={form.handleSubmit(onSubmit)}
+      className="space-y-3 w-1/2 px-7"
+    >
+      <Controller
+        name="username"
+        control={form.control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor={field.name}>ユーザー名</FieldLabel>
+            <Input
+              {...field}
+              id={field.name}
+              aria-invalid={fieldState.invalid}
+              placeholder="ユーザー名"
+              autoComplete="off"
+            />
+            {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+          </Field>
+        )}
+      />
+      <Controller
+        name="title"
+        control={form.control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor={field.name}>タイトル</FieldLabel>
+            <Input
+              {...field}
+              id={field.name}
+              aria-invalid={fieldState.invalid}
+              placeholder="タイトル"
+              autoComplete="off"
+            />
+            {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+          </Field>
+        )}
+      />
+      <Controller
+        name="content"
+        control={form.control}
+        render={({ field, fieldState }) => (
+          <Field data-invalid={fieldState.invalid}>
+            <FieldLabel htmlFor={field.name}>内容</FieldLabel>
+            <Textarea
+              {...field}
+              id={field.name}
+              aria-invalid={fieldState.invalid}
+              placeholder="投稿内容"
+              autoComplete="off"
+            />
+            {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+          </Field>
+        )}
+      />
+      <Button type="submit">Submit</Button>
+    </form>
+  );
 }
 
-export default createPage
+export default CreatePage;
